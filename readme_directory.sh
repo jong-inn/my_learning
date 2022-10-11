4c7905915c0d91daab52896471c15e75b5a2a727
jong-inn <bequiet13@gmail.com>
2022-10-10 21:05:19 -0500

    add comments on readme_directory.sh

--- readme_directory.sh
+++ readme_directory.sh
@@ -1,22 +1,30 @@
 
+# check whether README.md exists
 if test -f './README.md'
 then
+    # create new README.md
     rm README.md
     touch README.md
+    # header
     echo '# Welcome to my learning materials
 
 ### __Directories__' >> README.md
 else
+    # create new README.md if it doesn't exist
     touch README.md
+    # header
     echo '# Welcome to my learning materials
 
 ### __Directories__' >> README.md
 fi
 
+# add structure of directories using tree
 tree ./ >> README.md
+# delete summary sentence
 sed -i '' '$d
 ' README.md
 
+# add ``` to make tree as a code block
 sed -i '' '4i\
 ```
 ' README.md
