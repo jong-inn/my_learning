49d7c7658067238ab0f8981f92219e6f67ee7fc7
jong-inn <bequiet13@gmail.com>
2022-10-22 21:05:16 -0500

    add an udemy/algorithms_and_data_structures/python/binary_search_tree/BinarySearchTree.py file

---
+++ udemy/algorithms_and_data_structures/python/binary_search_tree/BinarySearchTree.py
@@ -0,0 +1,147 @@
+
+# Sources : Algorithms and Data Structures in Python (by Holczer Balazs on Udemy)
+
+import time
+
+class Node:
+    
+    def __init__(self, data, parent):
+        self.data = data
+        self.parent = parent
+        self.right_node = None
+        self.left_node = None
+
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def remove(self, data):
+        if self.root:
+            self.remove_node(data, self.root)
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data, None)
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        # we have to go to the left subtree
+        if data < node.data:
+            if node.left_node:
+                self.insert_node(data, node.left_node)
+            else:
+                node.left_node = Node(data, node)
+        # we have to visit the right subtree
+        else:
+            if node.right_node:
+                self.insert_node(data, node.right_node)
+            else:
+                node.right_node = Node(data, node)
+
+    def remove_node(self, data, node):
+
+        if node is None:
+            return
+
+        if data < node.data:
+            self.remove_node(data, node.left_node)
+        elif data > node.data:
+            self.remove_node(data, node.right_node)
+        else:
+
+            if node.left_node is None and node.right_node is None:
+                print(f"Removing a leaf node...{node.data}")
+
+                parent = node.parent
+
+                if parent is not None and parent.left_node == node:
+                    parent.left_node = None
+                if parent is not None and parent.right_node == node:
+                    parent.right_node = None
+
+                if parent is None:
+                    self.root = None
+
+                del node
+
+            elif node.left_node is None and node.right_node is not None:  # node !!!
+                print("Removing a node with single right child...")
+
+                parent = node.parent
+
+                if parent is not None:
+                    if parent.left_node == node:
+                        parent.left_node = node.right_node
+                    if parent.rightChild == node:
+                        parent.right_node = node.right_node
+                else:
+                    self.root = node.right_node
+
+                node.right_node.parent = parent
+                del node
+
+            elif node.right_node is None and node.left_node is not None:
+                print("Removing a node with single left child...")
+
+                parent = node.parent
+
+                if parent is not None:
+                    if parent.left_node == node:
+                        parent.left_node = node.left_node
+                    if parent.right_node == node:
+                        parent.right_node = node.left_node
+                else:
+                    self.root = node.left_node
+
+                node.left_node.parent = parent
+                del node
+
+            else:
+                print('Removing node with two children....')
+
+                predecessor = self.get_predecessor(node.left_node)
+
+                temp = predecessor.data
+                predecessor.data = node.data
+                node.data = temp
+
+                self.remove_node(data, predecessor)
+
+    def get_predecessor(self, node):
+        if node.right_node:
+            return self.get_predecessor(node.right_node)
+
+        return node
+
+    def traverse(self):
+        if self.root is not None:
+            self.traverse_in_order(self.root)
+
+    def traverse_in_order(self, node):
+
+        if node.left_node:
+            self.traverse_in_order(node.left_node)
+
+        print(node.data)
+
+        if node.right_node:
+            self.traverse_in_order(node.right_node)
+
+
+if __name__ == '__main__':
+
+    bst = BinarySearchTree()
+    start_time = time.time()
+    bst.insert(5000)
+    for i in range(0, 5000, 50):
+        bst.insert(i)
+        
+    for i in range(5000, 10000, 50):
+        bst.insert(i)
+
+    bst.traverse()
+    end_time = time.time()
+    print(f'Elapsed time : {round(end_time - start_time, 2)}')
